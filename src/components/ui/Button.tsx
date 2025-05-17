c766115c82f7fb2718c5b8dea5ad3a3757641e73
sabinabale <balejikovasabina@gmail.com>
2025-05-17 19:50:22 +0200

    update button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,16 +4,17 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { Slot } from "./Slot";
 
 const buttonVariants = cva(
-  "inline-flex w-fit items-center justify-center whitespace-nowrap rounded-lg flex-shrink-0 text-[14px] font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-40 transition-all duration-200 ease-in-out",
+  "inline-flex w-fit items-center justify-center whitespace-nowrap rounded-lg flex-shrink-0 text-[14px] font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-40 transition-all duration-200 ease-in-out cursor-pointer",
   {
     variants: {
       variant: {
         primary: "bg-black/90 text-white w-full hover:bg-[#333333] w-fit",
+        secondary: "bg-white text-black/70 w-full hover:text-cyan-600 w-fit",
         destructive: "text-red-600/70 hover:text-red-600",
-        icon: "bg-none border-none hover:opacity-40",
+        icon: "bg-none border-none",
       },
       size: {
-        default: "py-1.5 px-3",
+        default: "py-2 px-3",
         none: "p-0",
       },
     },
@@ -30,15 +31,31 @@ interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, onClick, ...rest }, ref) => {
-    const Comp = asChild ? Slot : "button";
+  (
+    { className, variant, size, asChild = false, onClick, children, ...rest },
+    ref
+  ) => {
+    if (asChild) {
+      return (
+        <Slot
+          className={cn(buttonVariants({ variant, size, className }))}
+          onClick={onClick}
+          {...rest}
+        >
+          {React.isValidElement(children) ? children : <span>{children}</span>}
+        </Slot>
+      );
+    }
+
     return (
-      <Comp
+      <button
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         onClick={onClick}
         {...rest}
-      />
+      >
+        {children}
+      </button>
     );
   }
 );
