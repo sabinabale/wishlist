5b338500d8b1fe749ba444644f5a216e7be087ac
sabinabale <balejikovasabina@gmail.com>
2025-05-17 19:50:42 +0200

    update layout for ProductItem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <Container>
-        <ProductItem />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <ProductItem />
+        </div>
       </Container>
     </div>
   );
