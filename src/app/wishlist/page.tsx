cefc69332a27912e1261759dd2c73a104ae9bf00
sabinabale <balejikovasabina@gmail.com>
2025-05-17 19:50:51 +0200

    set up wishlist page

---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/layout/Container";
+import WishlistTabs from "@/components/wishlist/WishlistTabs";
+import React from "react";
+
+export default function Page() {
+  return (
+    <Container>
+      <WishlistTabs />
+    </Container>
+  );
+}
