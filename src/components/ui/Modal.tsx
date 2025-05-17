b53ec674cf94516d3639d4d63aabe57383d33dab
sabinabale <balejikovasabina@gmail.com>
2025-05-17 20:23:55 +0200

    update modal to not focus button when escaped

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Button } from "./Button";
 import RemoveIcon from "../icons/RemoveIcon";
+import { createPortal } from "react-dom";
 
 interface ModalProps {
   children: React.ReactNode;
   onClose: () => void;
+  returnFocusOnClose?: boolean;
 }
 
-export default function Modal({ children, onClose }: ModalProps) {
-  React.useEffect(() => {
+export default function Modal({
+  children,
+  onClose,
+  returnFocusOnClose = false,
+}: ModalProps) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
     const handleEscapeKey = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
+        e.preventDefault();
         onClose();
+
+        if (!returnFocusOnClose) {
+          document.body.focus();
+
+          if (document.activeElement instanceof HTMLElement) {
+            document.activeElement.blur();
+          }
+        }
       }
     };
 
+    document.body.style.overflow = "hidden";
     document.addEventListener("keydown", handleEscapeKey);
-    return () => document.removeEventListener("keydown", handleEscapeKey);
-  }, [onClose]);
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black/30">
-      <div className="bg-white py-10 px-8 rounded-lg shadow-lg relative w-96">
+    if (modalRef.current) {
+      modalRef.current.focus();
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [onClose, returnFocusOnClose]);
+
+  return createPortal(
+    <div className="fixed inset-0 flex items-center justify-center bg-black/30 z-50">
+      <div
+        ref={modalRef}
+        className="bg-white py-10 px-8 rounded-lg shadow-lg relative w-96 z-50"
+        tabIndex={-1}
+      >
         <Button
           onClick={onClose}
           className="absolute top-4 right-4"
@@ -33,6 +63,7 @@ export default function Modal({ children, onClose }: ModalProps) {
         </Button>
         {children}
       </div>
-    </div>
+    </div>,
+    document.body
   );
 }
