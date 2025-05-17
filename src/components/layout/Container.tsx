ebbdba57a8b03bad9c0381f49dff63bf1dec1863
sabinabale <balejikovasabina@gmail.com>
2025-05-17 19:49:58 +0200

    update container styling

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="max-w-[1860px] mx-auto p-4 mt-20">{children}</div>;
+  return (
+    <div className="max-w-[1860px] w-full mx-auto p-4 mt-20">{children}</div>
+  );
 }
