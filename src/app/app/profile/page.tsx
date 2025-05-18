e08740ef4e5107c72eb2ed3c4f01c203e2279211
sabinabale <balejikovasabina@gmail.com>
2025-05-18 13:24:39 +0200

    set up profile page

--- src/app/app/profile/page.tsx
+++ src/app/app/profile/page.tsx
@@ -1,5 +1,12 @@
+import Container from "@/components/layout/Container";
 import React from "react";
 
 export default function Page() {
-  return <div>Profile</div>;
+  return (
+    <Container>
+      <h2>Profile</h2>
+      <div>Name: name</div>
+      <div>Email: email</div>
+    </Container>
+  );
 }
