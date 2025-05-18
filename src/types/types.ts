640e3ccbe47bc7c5256fea4be1bb2fe4e08b6993
sabinabale <balejikovasabina@gmail.com>
2025-05-18 17:13:22 +0200

    update types

--- src/types/types.ts
+++ src/types/types.ts
@@ -10,3 +10,31 @@ export interface User {
 export interface UsersData {
   users: User[];
 }
+
+export interface WishlistData {
+  id: string;
+  name: string;
+  description: string;
+  isPublic: boolean;
+  shareId?: string;
+  products: string[];
+  createdAt: string;
+}
+
+export interface WishlistsData {
+  [userId: string]: WishlistData[];
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  category: string;
+  sizes: number[];
+  price: number;
+  image: string;
+  inStock: number;
+}
+
+export interface ProductsData {
+  products: Product[];
+}
