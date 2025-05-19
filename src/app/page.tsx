61b44a48966891913a2ff5ccd41a136fffb98607
sabinabale <balejikovasabina@gmail.com>
2025-05-19 16:28:21 +0200

    add skeleton to products page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
+"use client";
+import { Suspense, useEffect, useState } from "react";
 import Container from "@/components/layout/Container";
 import ProductItem from "@/components/product/ProductItem";
+import { Product } from "@/types/types";
+import ProductSkeleton from "@/components/skeletons/Products";
 
 export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("/api/products");
+      const data = await response.json();
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="flex flex-col items-center">
-      <Container>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <ProductItem />
+    <Container>
+      <Suspense fallback={<ProductSkeleton />}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {products.map((product) => (
+            <ProductItem key={product.id} product={product} wishlistId="" />
+          ))}
         </div>
-      </Container>
-    </div>
+      </Suspense>
+    </Container>
   );
 }
